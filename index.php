7b4ed7dfe9e6e86d0256189155f2daa3043e9680
slon-ok <slon.jiv@ukr.net>
2018-09-07 12:02:26 +0300

    first

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>
+            <h1>Lessons</h1>
+            <p><a href="/lesson1/">Lesson 1</a></p>
+            <br /> 
+            <p><a href="/lesson2/">Lesson 2</a></p>
+        </div>
+    </body>
+</html>
---
+++ lesson1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>
+            <h1>Lesson 1</h1>
+            <blockquote>
+                <?php echo strftime('Сегодня %d-%m-%Y-%H-%M-%S'); ?>
+            </blockquote>
+            
+        </div>
+    </body>
+</html>
