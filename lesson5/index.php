023d9008eb22c4d04ddd154c80dde7fe7eae7a51
slon <slon@tech3>
2018-09-21 14:49:58 +0300

    Lesson5: Инкремент, декремент. Циклы, Foreach, таблица умножения

--- lesson5/index.php
+++ lesson5/index.php
@@ -47,6 +47,98 @@
                 echo "<br />";
                 
             ?>
+            
+            <h1>ЦИКЛЫ</h1>
+            <?php 
+                for($i =1; $i > 10; $i++){
+                    echo $i;
+                }
+                $i = 1;
+                
+                while($i < 10){
+                    echo $i;
+                    $i += 1;
+                }
+                
+                $str = "HELLO";
+                $i = 0;
+            ?>
+            
+            <p>
+                
+            <?php
+                $len = strlen($str); // не молоти в холостую!!! 
+                while($i < $len){       //посчитай строку заранее !!!
+                    echo $str{$i} . "<br />";
+                    $i += 1;
+                }
+                echo "<br />";
+                echo $str[4];
+                echo $str{4}; // идентичные команды
+                
+                $array = [1, 2, 3, 4];
+                echo $array[0];
+                echo $array[0]; // идентичные команды. 
+            ?>
+                
+            <h2>do-while</h2>
+            <?php    
+                $i = 1;
+                do{
+                    echo $i++;
+                }
+                while($i <= 10);    
+            ?>
+            
+            
+            <?php
+                while($k = 0){
+                    while($i < 9){
+                        $i++;
+                        if($i == 5) break 2; // выходит не только из этого цикла,
+                        echo $i;             // а и из предыдущего.  по умолчанию значение 1
+                    }                        // уровней должно быть не больше чем в реальности
+                }
+            ?>
+            <?php
+                while($i < 9){
+                    $i++;
+                    if($i == 5) Continue 1;
+                    echo $i;
+                }
+            ?>
+            </p>
+            
+            <h1>Laba 3.3</h1>
+            <?php
+            
+            $cols = 10;
+            $rows = 10;
+            $color = "blue";
+            
+            echo "<table style='border: 1px solid black;'>";
+            for($i = 1; $i <= $cols; $i++){
+                echo "<tr>";
+                for($j = 1; $j <= $rows; $j++){
+                    if($j==1 or $i == 1){
+                        echo "<th style='border: 1px solid black; background: $color;'>  " . $j * $i ."  </th>";
+                    } else{
+                        echo "<td style='border: 1px solid black;'>  " . $j * $i ."  </td>";
+                    } 
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+            
+            ?>
+            
+            <h2>Foreache</h2>
+            <?php 
+            $array = [1,5, "vanya"];
+            foreach($array as $key => $val){
+                    echo $key . " ---- " . $val . "<br />";
+            }
+            ?>
         <div>
     </body>
 </html>
